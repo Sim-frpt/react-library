a0e00647fda07f6abf9c9e25526055977ce321ce
Simon Fraipont <simon.fraipont@gmail.com>
2020-03-06 13:57:21 +0100

    polish book model and add default books to populate local storage

--- src/model/book.js
+++ src/model/book.js
@@ -1,15 +1,13 @@
 import placeHolder from "../assets/images/book-cover-placeholder.jpg";
 
-const books = [];
-
 const CreateBook = ({
+  id,
   title,
   author,
   pages,
   cover = placeHolder,
   isRead = false
 }) => {
-  const id = books.length ? books.length + 1 : 1;
   const self = {
     id,
     title,
@@ -22,27 +20,7 @@ const CreateBook = ({
     }
   };
 
-  updateBookArray(self);
-
   return self;
 };
 
-const updateBookArray = book => {
-  books.push(book);
-};
-
-CreateBook({
-  title: "test",
-  author: "myself",
-  pages: 423,
-  cover: placeHolder
-});
-
-CreateBook({
-  title: "test2",
-  author: "anotherAuthor",
-  pages: 123,
-  cover: placeHolder
-});
-
-export { books, CreateBook };
+export { CreateBook };
